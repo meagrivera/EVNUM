ed34a4edcfd6b8d09d3f4f8cb289e723633b7d4b
chepeadan <chepeadan@gmail.com>
2015-12-03 18:11:28 +0100

    visualization imporved - now single graph for users - manually created reduced graph

--- experiment_data/visualization.m
+++ experiment_data/visualization.m
@@ -1,7 +1,7 @@
 %This script visualizes the grid
 clear all
 %% VAriables
-ForPaper = 1; % (1) plots for paper (0) plots not for paper
+ForPaper = 0; % (1) plots for paper (0) plots not for paper
 
 
 
@@ -30,7 +30,7 @@ Lines = join(Lines, LineCodes,'key','LineCode');
 Loads.Properties.VariableNames{'Bus'}='Busname';
 Loads = join( Loads, Buscoords,'key', 'Busname' ) ;
 
-%% Graph construction
+%% Plot IEEE European LV Test Feeder
 
 %Edges start and terminal
 s = Lines{:,2};
@@ -39,63 +39,134 @@ t = Lines{:,3};
 %node labels
 nLabels = Buscoords{:,'Busname'}';
 eLabels = Lines{:,'Name'};
-weigths = Lines{:,'Length'} .* complex(Lines{:,'R1'}, Lines{:,'X1'});
+weigths = Lines{:,'Length'} .* 1e-3 .*  complex(Lines{:,'R1'}, Lines{:,'X1'});   %tranform length to [km]
 
 weigthsAbs= abs(weigths);
 
 
-%% Plot IEEE European LV Test Feeder
-% Contruct graph
+
+% Contruct original graph
 EdgeTable = table([s t], weigths , eLabels, 'VariableNames', {'EndNodes' 'Admittance' 'Name'} );
 NodeTable = Buscoords;
-G1=graph(EdgeTable,NodeTable)
+G1=graph(EdgeTable,NodeTable);
+
+% COnstruc graph that lead to loads
+tree = shortestpathtree(G1,Loads{:,'Busname'},1); % paths to load
+G2= graph(tree.Edges, tree.Nodes);
+indexTerminal = find(degree(G2)==1);
+indexJunction = find(degree(G2)>2);
+indexRouting = find(degree(G2)==2);
+
+% build new Graph with less nodes
+% snew= [1 25 27 27 25 32 36 36 32 59 66 66 59 101 155 155 171 188 188 171 196 196 241 101 114 127 127 127 145 261 261 145 283 283 114 247 247 263 263 280 373 373 391 530 530 391 505 505 578 578 594 594 615 615 666 786 786 854 854 666 686 690 690 686 691 691 707 891 891 707 718 732 721 718 745 763 763 745 794 884 884 794 863 863 280 310 336 336 310 325 325 332 332 453 453 475 484 484 475 508 544 544 508 560 651 651 560 588 588 ];
+% tnew= [25 27 70 34 32 36 47 83 59 66 73 74 101 155 178 171 188 208 264 196 225 241 248 114 127 289   145 261 276 314 283 320 327 247 387 263 342 280 373 458 391 530 539 556 505 522 578 785 594 614 615 688 666 786 817 854 860 861 686 690 778 701 691 702 707 891 900 896 718 732 755 813 745 763 780 835 794 884 898 906 863 886 899 310 336 388 349 325 337 332 406 453 629 475 484 502 563 508 544 611 562 560 651 682 676 588 619 639];
+% 
+
 
 
 % Plot
 figure
-%Full plot
-%p=plot(G1,'LineWidth',1,'NodeLabel',G1.Nodes.Busname,'EdgeLabel',G1.Edges.Name);
-% simple
-p=plot(G1,'LineWidth',2)
-title('Original IEEE European LV Test Feeder')
-axis([min(Buscoords{:,'x'})-10 max(Buscoords{:,'x'})+10 min(Buscoords{:,'y'}')-10 max(Buscoords{:,'y'}')+10])
-p.XData=Buscoords{:,'x'}'; 
-p.YData=Buscoords{:,'y'}';
-p.Marker = 'none'; % no marker on connection nodes
-highlight(p,Loads{:,'Busname'},'MarkerSize',5 ,'NodeColor','k', 'Marker','v') % highlight Loads
-highlight(p,1,'MarkerSize',8 ,'NodeColor','r', 'Marker','s')
-labelnode(p,1,'Transformer')
-labelnode(p,Loads{:,'Busname'}, Loads{:,'Name'} )
 if ForPaper== 1
-axis off
-set(gca,'position',[0 0 1 1],'units','normalized')
+    
+    
+    
+    p2=plot(G2,'LineWidth',2,'Marker','none');
+    p2.XData=Buscoords{G2.Nodes.Busname ,'x'}';
+    p2.YData=Buscoords{G2.Nodes.Busname ,'y'}';
+    
+    hold on
+    
+    p=plot(G1,'LineStyle','--');
+    axis off
+    set(gca,'position',[0 0 1 1],'units','normalized')
+    
+    
+    axis([min(Buscoords{:,'x'})-10 max(Buscoords{:,'x'})+10 min(Buscoords{:,'y'}')-10 max(Buscoords{:,'y'}')+10])
+    p.XData=Buscoords{:,'x'}';
+    p.YData=Buscoords{:,'y'}';
+    p.Marker = 'none'; % no marker on connection nodes
+    highlight(p,Loads{:,'Busname'},'MarkerSize',5 ,'NodeColor','k', 'Marker','v') % highlight Loads
+    highlight(p,1,'MarkerSize',8 ,'NodeColor','r', 'Marker','s')
+    labelnode(p,1,'Transformer')
+    labelnode(p,Loads{:,'Busname'}, Loads{:,'Name'} )
+
+    
+else
+    p=plot(G1,'LineWidth',2,'NodeLabel',G1.Nodes.Busname);
+    title('Original IEEE European LV Test Feeder')
+    axis([min(Buscoords{:,'x'})-10 max(Buscoords{:,'x'})+10 min(Buscoords{:,'y'}')-10 max(Buscoords{:,'y'}')+10])
+    p.XData=Buscoords{:,'x'}';
+    p.YData=Buscoords{:,'y'}';
+    %p.Marker = 'none'; % no marker on connection nodes
+    %highlight(p,Loads{:,'Busname'},'MarkerSize',5 ,'NodeColor','k', 'Marker','v') % highlight Loads
+    highlight(p,indexTerminal,'MarkerSize',10 ,'NodeColor','g')
+    highlight(p,1,'MarkerSize',8 ,'NodeColor','r', 'Marker','s')
+    highlight(p,indexJunction,'MarkerSize',10 ,'NodeColor','r')
+    labelnode(p,1,'Transformer')
+    %labelnode(p,Loads{:,'Busname'}, Loads{:,'Name'} )
+  
+    
+    
+%     
+%     
+%     highlight(p,indexRouting,'MarkerSize',10 ,'NodeColor','b')
+    
+    
 end
+
+
+
+%% Calculating new test grid for experimetns
+% Contruct graph to loads
+% tree = shortestpathtree(G1,Loads{:,'Busname'},1); % paths to load
+% G2= graph(tree.Edges, tree.Nodes); 
+% 
+% 
+% p2=plot(G2,'LineWidth',2,'Marker','none','EdgeColor','b');
+% p2.XData=Buscoords{G2.Nodes.Busname ,'x'}';
+% p2.YData=Buscoords{G2.Nodes.Busname ,'y'}';
+% 
+% 
+% 
+% indexTerminal = find(degree(G2)==1);
+% indexJunction = find(degree(G2)>2);
+% indexRouting = find(degree(G2)==2);
+% 
+% highlight(p2,indexJunction,'MarkerSize',10 ,'NodeColor','r')
+% highlight(p2,indexTerminal,'MarkerSize',10 ,'NodeColor','g')
+% highlight(p2,indexRouting,'MarkerSize',10 ,'NodeColor','b')
+% 
+
+
+
 
 %% Plot Simplified IEEE European LV Test Feeder
+% 
 
-% Contruct graph
-tree = shortestpathtree(G1,Loads{:,'Busname'},1); % paths to load
-G2= graph(tree.Edges, tree.Nodes); 
+% 
+% % Plot
+% figure
+% p2=plot(G2,'LineWidth',2);
+% title('Modified IEEE European LV Test Feeder');
+% axis([min(Buscoords{:,'x'})-10 max(Buscoords{:,'x'})+10 min(Buscoords{:,'y'}')-10 max(Buscoords{:,'y'}')+10]);
+% p2.XData=Buscoords{G2.Nodes.Busname ,'x'}';
+% p2.YData=Buscoords{G2.Nodes.Busname ,'y'}';
+% p2.Marker = 'none'; % no marker on connection nodes
+% highlight(p2,Loads{:,'Busname'},'MarkerSize',5 ,'NodeColor','k', 'Marker','v') % highlight Loads
+% highlight(p2,1,'MarkerSize',8 ,'NodeColor','r', 'Marker','s')
+% labelnode(p2,1,'Transformer')
+% labelnode(p2,Loads{:,'Busname'}, Loads{:,'Name'} )
+% if ForPaper== 1
+% axis off
+% set(gca,'position',[0 0 1 1],'units','normalized')
+% end
+% 
 
-% Plot
-figure
-p2=plot(G2,'LineWidth',2)
-title('Modified IEEE European LV Test Feeder')
-axis([min(Buscoords{:,'x'})-10 max(Buscoords{:,'x'})+10 min(Buscoords{:,'y'}')-10 max(Buscoords{:,'y'}')+10])
-p2.XData=Buscoords{G2.Nodes.Busname ,'x'}';
-p2.YData=Buscoords{G2.Nodes.Busname ,'y'}';
-p2.Marker = 'none'; % no marker on connection nodes
-highlight(p2,Loads{:,'Busname'},'MarkerSize',5 ,'NodeColor','k', 'Marker','v') % highlight Loads
-highlight(p2,1,'MarkerSize',8 ,'NodeColor','r', 'Marker','s')
-labelnode(p2,1,'Transformer')
-labelnode(p2,Loads{:,'Busname'}, Loads{:,'Name'} )
-if ForPaper== 1
-axis off
-set(gca,'position',[0 0 1 1],'units','normalized')
-end
 
-%% Clean up nodes without connection
-%G2 = rmnode(G2,find(degree(G2)==0)) ;
+
+% %% Clean up nodes without connection
+%G2 = G2% rmnode(G2,find(degree(G2)==0)) ;
+
 
 
 
