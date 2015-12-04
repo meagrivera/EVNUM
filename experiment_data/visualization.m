245934e16fca5f60dfa9c2c5d9f4658f44f2cf89
chepeadan <chepeadan@gmail.com>
2015-12-04 16:17:21 +0100

    new manually imputed new edges and calculculated newAdmittances on new lines - missing generate graph from this data

--- experiment_data/visualization.m
+++ experiment_data/visualization.m
@@ -3,6 +3,7 @@ clear all
 %% VAriables
 ForPaper = 0; % (1) plots for paper (0) plots not for paper
 
+%Define Ampacity of line types 
 
 
 
@@ -30,7 +31,7 @@ Lines = join(Lines, LineCodes,'key','LineCode');
 Loads.Properties.VariableNames{'Bus'}='Busname';
 Loads = join( Loads, Buscoords,'key', 'Busname' ) ;
 
-%% Plot IEEE European LV Test Feeder
+%% Regularize IEEE European LV Test Feeder data for EVNUM
 
 %Edges start and terminal
 s = Lines{:,2};
@@ -50,21 +51,36 @@ EdgeTable = table([s t], weigths , eLabels, 'VariableNames', {'EndNodes' 'Admitt
 NodeTable = Buscoords;
 G1=graph(EdgeTable,NodeTable);
 
-% COnstruc graph that lead to loads
+% Construc graph only with paths to loads
 tree = shortestpathtree(G1,Loads{:,'Busname'},1); % paths to load
 G2= graph(tree.Edges, tree.Nodes);
 indexTerminal = find(degree(G2)==1);
 indexJunction = find(degree(G2)>2);
 indexRouting = find(degree(G2)==2);
+indexNewNodes= [indexTerminal; indexJunction]
 
-% build new Graph with less nodes
-% snew= [1 25 27 27 25 32 36 36 32 59 66 66 59 101 155 155 171 188 188 171 196 196 241 101 114 127 127 127 145 261 261 145 283 283 114 247 247 263 263 280 373 373 391 530 530 391 505 505 578 578 594 594 615 615 666 786 786 854 854 666 686 690 690 686 691 691 707 891 891 707 718 732 721 718 745 763 763 745 794 884 884 794 863 863 280 310 336 336 310 325 325 332 332 453 453 475 484 484 475 508 544 544 508 560 651 651 560 588 588 ];
-% tnew= [25 27 70 34 32 36 47 83 59 66 73 74 101 155 178 171 188 208 264 196 225 241 248 114 127 289   145 261 276 314 283 320 327 247 387 263 342 280 373 458 391 530 539 556 505 522 578 785 594 614 615 688 666 786 817 854 860 861 686 690 778 701 691 702 707 891 900 896 718 732 755 813 745 763 780 835 794 884 898 906 863 886 899 310 336 388 349 325 337 332 406 453 629 475 484 502 563 508 544 611 562 560 651 682 676 588 619 639];
-% 
+% build new Graph with less nodes (use option ForPaper=0 to verify visually )
+snew= [1  25 27 27 25 32 36 36 32 59 66 66 59  101 155 155 171 188 188 171 196 196 241 101 114 127 127 145 261 261 145 283 283 114 247 247 263 263 280 373 373 391 530 530 391 505 505 578 578 594 594 615 615 666 786 786 854 854 666 686 690 690 686 691 707 891 891 707 718 739 739 718 745 763 763 745 794 884 884 794 868 868 280 310 336 336 310 325 325 332 332 453 453 475 484 484 475 508 544 544 508 559 651 651 559 587 587];
+tnew= [25 27 70 34 32 36 47 83 59 66 73 74 101 155 178 171 188 208 264 196 225 241 248 114 127 289 145 261 314 276 283 327 320 247 387 263 342 280 373 458 391 530 556 539 505 522 578 785 594 614 615 688 666 786 817 854 861 860 686 690 701 778 691 707 891 896 900 718 739 813 755 745 763 835 780 794 884 906 898 868 899 886 310 336 349 388 325 337 332 406 453 629 475 484 563 502 508 544 611 562 559 651 676 682 587 619 639];
+
+Greal = graph(s,t, real(weigths) );
+Gimag = graph (s, t, imag(weigths));
+
+newR= distances(Greal);
+newR= diag( newR( snew,tnew ) );
+
+newX= distances( Gimag );
+newX= diag( newX( snew,tnew ) );
+
+newWeigths = complex( newR, newX );
+
+% Create simplified graph
+EdgeTableNew = table([snew' tnew'], newWeigths , 'VariableNames', {'EndNodes' 'Admittance'} );
+NodeTableNew = Buscoords(indexNewNodes,:);
+Gsimple=graph(EdgeTableNew,NodeTableNew);
 
 
-
-% Plot
+%% Plot
 figure
 if ForPaper== 1
     
@@ -92,11 +108,13 @@ if ForPaper== 1
 
     
 else
-    p=plot(G1,'LineWidth',2,'NodeLabel',G1.Nodes.Busname);
+    p=plot(G1,'LineWidth',2);
     title('Original IEEE European LV Test Feeder')
     axis([min(Buscoords{:,'x'})-10 max(Buscoords{:,'x'})+10 min(Buscoords{:,'y'}')-10 max(Buscoords{:,'y'}')+10])
     p.XData=Buscoords{:,'x'}';
     p.YData=Buscoords{:,'y'}';
+    labelnode(p,indexTerminal, G1.Nodes.Busname(indexTerminal));
+    labelnode(p,indexJunction, G1.Nodes.Busname(indexJunction));
     %p.Marker = 'none'; % no marker on connection nodes
     %highlight(p,Loads{:,'Busname'},'MarkerSize',5 ,'NodeColor','k', 'Marker','v') % highlight Loads
     highlight(p,indexTerminal,'MarkerSize',10 ,'NodeColor','g')
